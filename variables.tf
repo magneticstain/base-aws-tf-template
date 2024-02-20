c44b303385dbdad67118e373de8c7d72e5ed9b4e
Josh Carlson <837837+magneticstain@users.noreply.github.com>
2024-02-20 17:01:01 -0500

    Add example tf vars file

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "var_1" {
+    type = string
+    description = "Example variable 1"
+    default = ""
+    nullable = false
+}
+
+variable "var_1" {
+    type = map(string)
+    description = "Example variable 2"
+    default = {}
+}
