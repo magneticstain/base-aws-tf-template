291fda9f83d81a55111368271375eecea5f88c69
Josh Carlson <837837+magneticstain@users.noreply.github.com>
2024-02-20 17:09:27 -0500

    Format variables file

--- variables.tf
+++ variables.tf
@@ -1,9 +1,5 @@
 variable "accounts" {
     type = map(string)
     description = "Mapping of aliases->IAM roles of accounts to rollout plans to"
-    default = {
-        "jcarlsonpurcell-personal" = "arn:aws:iam::509915386432:role/admin-cli",
-        "jcarlsonpurcell-personal-testing" = "arn:aws:iam::138277128026:role/admin-cli",
-        "jcarlsonpurcell-personal-testing-2" = "arn:aws:iam::685680125206:role/admin-cli"
-    }
+    default = {}
 }
